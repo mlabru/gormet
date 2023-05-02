db67959aad2566814b833c08fc616dd3400cff50
Milton Abrunhosa <ml_byteman@yahoo.com.br>
2023-05-02 13:10:11 -0300

    shell script

---
+++ gormet.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# language
+# export LANGUAGE=pt_BR
+
+# gormet directory
+GORMET=~/Public/gormet
+
+# nome do computador
+HOST=`hostname`
+
+# get today's date
+TDATE=`date '+%Y-%m-%d_%H-%M-%S'`
+
+# home directory exists ?
+if [ -d ${GORMET} ]; then
+    # set home dir
+    cd ${GORMET}
+fi
+
+# log file
+LOGF="logs/gormet.$HOST.$TDATE.log"
+
+# logger
+echo "Início de processamento: " $(date '+%Y-%m-%d %H:%M') > $LOGF
+
+# ckeck if another instance of loader is running
+DI_PID_GORMET=`ps ax | grep -w python3 | grep -w gormet.py | awk '{ print $1 }'`
+
+if [ ! -z "$DI_PID_GORMET" ]; then
+    # log warning
+    echo "[`date`]: process gormet is already running. Waiting..." >> $LOGF
+    # kill process
+    kill -9 $DI_PID_GORMET
+    # wait 10s
+    sleep 10
+fi
+
+# set PYTHONPATH
+export PYTHONPATH="$PWD/."
+# executa o loader
+poetry run python3 gormet.py $@ >> $LOGF 2>&1
+
+# logger
+echo "Fim de processamento: " $(date '+%Y-%m-%d %H:%M') >> $LOGF
+
+# < the end >----------------------------------------------------------------------------------
